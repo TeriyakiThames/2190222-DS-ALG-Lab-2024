cc61975982d96b9783fb7e25a167777d6debba8e
Teriyaki Thames <64459916+TeriyakiThames@users.noreply.github.com>
2024-10-16 21:12:51 +0700

    Update StackUtility.java
    Changed from using methods from array/list to just iterating in a while loop instead.

--- DSAlg Lab/Midterms/Lab 6.2/src/StackUtility.java
+++ DSAlg Lab/Midterms/Lab 6.2/src/StackUtility.java
@@ -1,24 +1,18 @@
 public class StackUtility {
 	public static MyStack removeRange(MyStack s, int i, int j) throws Exception {
 		StackArray tempStack = new StackArray();
-		int stackSize = 0;
+		int index = 0;
 
-		// Get the size from the stack
-		if (s instanceof StackArray) {
-			stackSize = ((StackArray) s).getCurrentSize();
-		} else if (s instanceof StackLinkedList) {
-			stackSize = ((StackLinkedList) s).getTheList().size();
-		}
-		
 		// Move elements outside range to a new stack
-		for (int index = 0; index < stackSize; index++) {
+		while (!s.isEmpty()) {
 			int topElement = s.top();
 			s.pop();
 			if (index < i || index > j) {
 				tempStack.push(topElement);
 			}
+			index++;
 		}
-		
+
 		// Move the elements back to the old stack
 		while (!tempStack.isEmpty()) {
 			int data = tempStack.top();
